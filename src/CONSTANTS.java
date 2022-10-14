9a1aa8b7030af89255aa5b4c70999e98934f6f0c
galgimel <samokhina.galya.15@gmail.com>
2022-10-14 17:31:22 +0300

    ReverseWords

---
+++ src/CONSTANTS.java
@@ -0,0 +1,4 @@
+public class CONSTANTS {
+    public final static String SPLIT_STRING = " ";
+    public final static String EMPTY_STRING = "";
+}
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,14 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
-
 public class Main {
-    public final static String SPLIT_STRING = " ";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите что-нибудь: ");
         String input = scanner.nextLine().trim();
-        String[] words = input.split(SPLIT_STRING);
+        String[] words = input.split(CONSTANTS.SPLIT_STRING);
         new Reverse().printReverseWords(words);
-    }
-}
-
-class Reverse {
-    public void printReverseWords(String[] words) {
-        for (String word : words) {
-            String reversedWord = reverse(word);
-            System.out.print(reversedWord + " ");
-        }
-    }
-
-    private String reverse(String word) {
-        char[] copyWord = word.toCharArray();
-        String toString = "";
-        StringBuilder reversedWord = new StringBuilder(toString);
-        for (int i = word.length() - 1; i > -1; i--) {
-            if (Character.isLetter(copyWord[i])) {
-                reversedWord.append(copyWord[i]);
-            }
-        }
-        for (int i = 0; i < word.length(); i++) {
-            if (!(Character.isLetter(copyWord[i]))) {
-                reversedWord.insert(i, copyWord[i]);
-            }
-        }
-        toString = reversedWord.toString();
-        return toString;
-
     }
 
 }
