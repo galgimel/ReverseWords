ad634854fa382126044d58ab111c03923ca0acbe
galgimel <samokhina.galya.15@gmail.com>
2022-10-14 15:56:49 +0300

    ReverseWords

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите данные: ");
+        System.out.println("Введите что-нибудь: ");
         String input = scanner.nextLine().trim();
         String[] words = input.split(SPLIT_STRING);
         new Reverse().printReverseWords(words);
